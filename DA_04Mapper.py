4ea69779bf3abab12222c0247563ba19c47cfd54
TheOneFluffer <90469659+TheOneFluffer@users.noreply.github.com>
2022-02-15 16:05:08 +0800

    DA_04Mapper.py
    Finished task 6 of the project
    i. Use Scrapy web-crawler with appropriate parser “response.css”
    ii. Display reference webpage
    iii. Store the retrieve information in JSON (results.json)

---
+++ DA_04Mapper.py
@@ -0,0 +1,14 @@
+# Importing modules
+import scrapy
+
+class Crawl(scrapy.Spider):
+    name = "new_spider"
+    start_urls = ['https://brickset.com/sets/year-2013']
+
+    def parse(self, response):
+        css_selector = 'img'
+        for x in response.css(css_selector):
+            newsel = '@src'
+            yield {
+                'Image Link': x.xpath(newsel).extract_first(),
+            }
