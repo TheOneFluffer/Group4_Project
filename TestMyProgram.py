1e8d43bee98fcd8552299bc5cd8cf128d509722c
h4lphas <93511044+h4lphas@users.noreply.github.com>
2022-02-20 18:54:29 +0800

    Add files via upload
    Tasks Done

---
+++ TestMyProgram.py
@@ -0,0 +1,9 @@
+#This is to import the unittest program
+import unittest
+# This is to test the following application specifically to a particular sets of input
+class scrapyTest(unittest.TestCase):
+    def test_EngineType(self):
+        print("Testing")
+       #to execute command from this module
+if __name__ == '__main__':
+    untitest.main()
